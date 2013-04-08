9ce7a410692a474c6a51add1fcf63df2bd0b6a06
Andrey Chislenko <linuxsysprog@yandex.com>
2013-04-08 20:23:38 +0000

    Rewrote rm_sfk.bat in Perl.
    git-svn-id: file:///cygdrive/c/tmp/nf1/My_Music_repo@5 c5d87c15-78e7-1c46-8049-92da82b6d476

---
+++ My_Music/rm_sfk.pl
@@ -0,0 +1,54 @@
+=head1
+
+A rm_sfk.bat rewrite in Perl.
+Given a folder removes its files recursively which end with one of the extensions from the list.
+The input to the script is a folder. The script works on files only.
+
+x Install script into the Windows Explorer context menu
+	x Test with a x) file x) folder ?) current directory
+	/ Test with !) non-English filenames and x) spaces
+? Check input to be a folder
+x Remove files with File::Find x) find only files. For each file:
+	x Check for error x) Print error
+? Report total number of deleted files
+x Pause script
+
+=cut
+
+use Data::Dumper;
+use File::Find;
+
+# Not a subroutine reference at C:/strawberry/perl/vendor/lib/File/Find/Rule.pm line 73.
+# use File::Find::Rule;
+
+my @rm_exts = qw/
+BAK
+DLL
+EXE
+PK
+SFK
+SFL
+/;
+
+find({ wanted => \&wanted }, $ARGV[0]);
+
+<STDIN>;
+
+sub wanted {
+	if (! -f) {
+		return;
+	}
+	
+	foreach my $ext (@rm_exts) {
+		if ($File::Find::name =~ /\.${ext}$/i) {
+			if (unlink $File::Find::name) {
+				print "Removed $File::Find::name\n";
+			} else {
+				warn "Failed to remove $File::Find::name: $!";
+			}
+			
+			return;
+		}
+	}
+}
+
