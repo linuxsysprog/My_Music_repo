2581652fb96e97155dfe89104af065a539c183a0
Andrey Chislenko <linuxsysprog@yandex.com>
2013-05-10 10:18:41 +0000

    An (almost) complete rewrite of the script.
    git-svn-id: file:///cygdrive/c/tmp/nf1/My_Music_repo@8 c5d87c15-78e7-1c46-8049-92da82b6d476

--- My_Music/rm_sfk.pl
+++ My_Music/rm_sfk.pl
@@ -1,53 +1,85 @@
 =head1
 
 A rm_sfk.bat rewrite in Perl.
-Given a folder removes its files recursively which end with one of the extensions from the list.
-The input to the script is a folder. The script works on files only.
+Deletes files with certain extensions from a directory recursing into subdirs.
 
 x Install script into the Windows Explorer context menu
 	x Test with a x) file x) folder ?) current directory
 	/ Test with !) non-English filenames and x) spaces
-? Check input to be a folder
-x Remove files with File::Find x) find only files. For each file:
-	x Check for error x) Print error
-x Pause script
-- add counters
-- re-test
 
 =cut
 
 use Common;
 use File::Find;
-# Not a subroutine reference at C:/strawberry/perl/vendor/lib/File/Find/Rule.pm line 73.
-# use File::Find::Rule;
+use Cwd;
+
+if (! -d $ARGV[0]) {
+	print $ARGV[0], " is not a directory\n";
+	exit 1;
+}
 
 my @rm_exts = qw/
-BAK
-DLL
-EXE
-PK
-SFK
-SFL
+bak
+dll
+exe
+pk
+sfk
+sfl
 /;
 
-find({ wanted => \&wanted }, $ARGV[0]);
+# Reset counters
+my $entries = 0;
+my $plain_files = 0;
+my $matching_files = 0;
+my $unlinked = 0;
+my $failed = 0;
+
+print "Cleaning ", uc join(' ', sort @rm_exts), " out of ", getcwd(), '/', $ARGV[0], "\n";
+
+find(\&wanted, $ARGV[0]);
+
+# Print counters
+print "Total entries (files and dirs) found: $entries\n";
+print "Plain files found: $plain_files\n";
+print "Files matching a pattern found: $matching_files\n";
+if ($matching_files > 0) {
+	print "Files successfully unlinked: $unlinked\n";
+	print "Files failed to unlink: $failed\n";
+} else {
+	print "Nothing to unlink.\n";
+}
+
 Common::pause();
 
 sub wanted {
-	if (! -f $File::Find::name) {
+	$entries++;
+	
+	my $file = getcwd() . '/' . $_;
+	
+	if (! -f $file) {
 		return;
 	}
+	$plain_files++;
 	
+	my $matched = 0;
 	foreach my $ext (@rm_exts) {
-		if ($File::Find::name =~ /\.${ext}$/i) {
-			if (unlink $File::Find::name) {
-				print "Removed $File::Find::name\n";
-			} else {
-				warn "Failed to remove $File::Find::name: $!";
-			}
-			
-			return;
+		if ($file =~ /\.${ext}$/i) {
+			$matched = 1;
+			last;
 		}
+	}
+	if (!$matched) {
+		return;
+	}
+	$matching_files++;
+	
+	my $ret = unlink $file;
+	if ($ret) {
+		$unlinked++;
+		print "unlinked $file\n"
+	} else {
+		$failed++;
+		print "failed to unlink $file: $!\n"
 	}
 }
 
