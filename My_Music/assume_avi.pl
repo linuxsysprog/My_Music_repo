23869122f52feb5aaf9ba957d69890287ee42cf0
Andrey Chislenko <linuxsysprog@yandex.com>
2013-07-17 17:33:42 +0000

    Changed script to vo.avi extension.
    git-svn-id: file:///cygdrive/c/tmp/nf1/My_Music_repo@19 c5d87c15-78e7-1c46-8049-92da82b6d476

--- My_Music/assume_avi.pl
+++ My_Music/assume_avi.pl
@@ -1,6 +1,6 @@
 =head1
 
-Slows down AVI files in a folder.
+Slows down VO.AVI files in a folder.
 The script does not recurse into subdirs.
 
 =cut
@@ -13,22 +13,22 @@ if ($ARGV[1] eq 'gui') {
 	$dir = $ARGV[0];
 }
 
-Common::find(\&wanted, $dir, '\.avi$');
+Common::find(\&wanted, $dir, '\.vo.avi$');
 Common::pause;
 
 sub wanted {
 	my $file = shift;
-	$file =~ s/\.avi$//;
+	$file =~ s/\.vo.avi$//;
 	
 	foreach my $percentage (@Common::percentages) {
 		my $fps = $Common::fps * ($percentage/100);
 		
 		my $avis_contents = <<END;
-AviSource("$dir/$file.avi")
+AviSource("$dir/$file.vo.avi")
 AssumeFPS($fps)
 END
 
-		my $vdub_contents = Common::get_vdub_contents($dir, $file, ".$percentage.avi");
+		my $vdub_contents = Common::get_vdub_contents($dir, $file, ".$percentage.vo.avi");
 		
 		Common::create_config_file("$dir/$Common::avis_config", $avis_contents);
 		Common::create_config_file("$dir/$Common::vdub_config", $vdub_contents);
